0f48218aba142c27edec5e7569ed1a933cfa161a
patrice jaszczynski <ppastg92@free.fr>
2020-02-12 15:28:17 +0100

    Yelp MVP

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  CAT = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: {in: CAT }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+epicure = { name: "Epicure", category: "french", address: "4 rue de la pompe", phone_number: "0102930201" }
+mcdo = { name: "Mcdo", category: "belgian", address: "rue oberkampf", phone_number: "0937291828" }
+love = { name: "Love", category: "french", address: "rue de l'amour", phone_number: "0147263517" }
+pizza = { name: "pizza hut", category: "italian", address: "boulevard de l'opera", phone_number: "0102034050" }
+japan = { name: "Japan_food", category: "japanese", address: "rue de la paix", phone_number: "0989273892" }
+
+[ epicure, mcdo, love, pizza, japan ].each do |name|
+  restaurant = Restaurant.create!(name)
+end
